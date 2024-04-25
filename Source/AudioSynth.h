b40b23daca2bed98efcca96218afa0403ecefa26
La-Peticchia <134319270+La-Peticchia@users.noreply.github.com>
2024-04-25 12:52:12 +0200

    Aggiunta distorsione con il waveshaping
    Ora serve uno slider per modificare il gain e il bias nella classe CustomDistortion

--- Source/AudioSynth.h
+++ Source/AudioSynth.h
@@ -9,75 +9,12 @@
 */
 
 #include <JuceHeader.h>
+#include "CustomOscillator.h"
+#include "CustomDistortion.h"
 
 #pragma once
 
-template <typename Type>
-class CustomOscillator : juce::dsp::Oscillator<Type> {
 
-public:
-
-    CustomOscillator() {
-
-        switch (currentState) {
-
-        case sawThooth:
-            osc.initialise([](Type x)
-                {
-                    return juce::jmap(x,
-                    Type(-juce::MathConstants<double>::pi),
-                    Type(juce::MathConstants<double>::pi),
-                    Type(-1),
-                    Type(1));
-
-                }, 2);
-            break;
-
-        }
-    }
-
-    void setFrequency(Type newValue, bool force = false)
-    {
-        osc.setFrequency(newValue, force);     // [7]
-    }
-
-    //==============================================================================
-    //void setLevel(Type newValue)
-    //{
-    //    auto& gain = processorChain.template get<gainIndex>();
-    //    gain.setGainLinear(newValue);          // [8]
-    //}
-
-    //==============================================================================
-    void reset() noexcept
-    {
-        osc.reset(); // [4]
-    }
-
-    //==============================================================================
-    template <typename ProcessContext>
-    void process(const ProcessContext& context) noexcept
-    {
-        osc.process(context);       // [9]
-    }
-
-    //==============================================================================
-    void prepare(const juce::dsp::ProcessSpec& spec)
-    {
-        osc.prepare(spec); // [3]
-    }
-
-
-    enum WaveType {
-        sawThooth,
-        squareWave,
-        triangular
-    } currentState = sawThooth;
-
-private:
-    //juce::dsp::ProcessorChain<juce::dsp::Oscillator<Type>, juce::dsp::Gain<Type>> processorChain;
-    juce::dsp::Oscillator<Type> osc;
-};
 
 struct Sound : public juce::SynthesiserSound
 {
@@ -110,7 +47,9 @@ class Voice : public juce::SynthesiserVoice {
     
         void stopNote(float /*velocity*/, bool allowTailOff) override
         {
+            processorChain.get<gainIndex>().setGainLinear(0);
             clearCurrentNote();
+
         }
 
         void prepare(const juce::dsp::ProcessSpec& spec)
@@ -139,10 +78,19 @@ class Voice : public juce::SynthesiserVoice {
         enum
         {
             oscIndex,
+            distortionIndex,
             gainIndex
         };
+        
+        juce::dsp::ProcessorChain<CustomOscillator<float>, CustomDistortion<float>, juce::dsp::Gain<float>> processorChain;
+        
+        //enum
+        //{
+        //    oscIndex,
+        //    gainIndex
+        //};
+        //juce::dsp::ProcessorChain<CustomOscillator<float>, juce::dsp::Gain<float>> processorChain;
 
-        juce::dsp::ProcessorChain<CustomOscillator<float>, juce::dsp::Gain<float>> processorChain;
         juce::dsp::AudioBlock<float> tempBlock;
         juce::HeapBlock<char> heapBlock;
 
---
+++ Source/CustomOscillator.h
@@ -0,0 +1,92 @@
+/*
+  ==============================================================================
+
+    CustomOscillator.h
+    Created: 25 Apr 2024 8:30:57am
+    Author:  Slend
+
+  ==============================================================================
+*/
+
+#pragma once
+
+enum WaveType {
+    sine,
+    sawThooth,
+    squareWave,
+    triangular
+};
+
+template <typename Type>
+class CustomOscillator{
+
+public:
+
+    CustomOscillator() {
+        SetWave(sine);
+    }
+
+    void SetWave(WaveType waveT) {
+
+        switch (waveT) {
+
+        case sine:
+            osc.initialise([](Type x) { return std::sin(x); }, 128);
+            break;
+
+        case sawThooth:
+            osc.initialise([](Type x)
+                {
+                    return juce::jmap(x,
+                    Type(-juce::MathConstants<double>::pi),
+                    Type(juce::MathConstants<double>::pi),
+                    Type(-1),
+                    Type(1));
+
+                }, 2);
+            break;                                                 
+
+        case triangular:
+            osc.initialise([](Type x) {
+
+                auto val = juce::jmap(x,
+                Type(-juce::MathConstants<double>::pi),
+                Type(juce::MathConstants<double>::pi),
+                Type(-1),
+                Type(1));
+
+                return Type(2) * std::fabs(fmod(val, Type(2)) - Type(1)) - Type(1);
+
+                }, 128);
+            break;
+
+        }
+
+    }
+
+    void setFrequency(Type newValue, bool force = false)
+    {
+        osc.setFrequency(newValue, force);     // [7]
+    }
+
+
+    void reset() noexcept
+    {
+        osc.reset(); // [4]
+    }
+
+    template <typename ProcessContext>
+    void process(const ProcessContext& context) noexcept
+    {
+        osc.process(context);       // [9]
+    }
+
+    void prepare(const juce::dsp::ProcessSpec& spec)
+    {
+        osc.prepare(spec); // [3]
+    }
+
+
+private:
+    juce::dsp::Oscillator<Type> osc;
+};
