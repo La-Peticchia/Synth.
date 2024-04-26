063d63f8721269b80f6404aa0ba2892adb7826b1
EdoardoColucci03 <edoardocolucci03@gmail.com>
2024-04-26 17:43:18 +0200

    group component

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -18,17 +18,26 @@ GCB_SynthAudioProcessorEditor::GCB_SynthAudioProcessorEditor (GCB_SynthAudioProc
     addAndMakeVisible(keyboardComp);
     keyboardComp.setMidiChannel(2);
 
+    //CustomLookAndFeel customLookAndFeel;
+
+    //aggiungo lo slider
     addAndMakeVisible(knob);
-    knob.setSliderStyle(juce::Slider::SliderStyle::RotaryVerticalDrag);
+   // knob.setLookAndFeel(&customLookAndFeel);
+    knob.setSliderStyle(juce::Slider::SliderStyle::Rotary);
     knob.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 64, 32);
-    knob.setRange(-24.0, 24.0, 0.1);
+    knob.setRange(0, 3, 1.5);
     knob.setColour(juce::Slider::ColourIds::rotarySliderFillColourId, juce::Colours::white);
 
+    addAndMakeVisible(border);
+    border.setText("Tipo_forma_d'onda");
+
+
+
     //metodi per resizare la window
-    getConstrainer()->setFixedAspectRatio(2.0);
-    setResizable(true, true);
-    setResizeLimits(500,250,1200,600);
-    setSize (600, 500);
+    //getConstrainer()->setFixedAspectRatio(2.0);
+    //setResizable(true, true);
+    //setResizeLimits(500,250,1200,600);
+    setSize (1000, 550);
 }
 
 GCB_SynthAudioProcessorEditor::~GCB_SynthAudioProcessorEditor()
@@ -39,7 +48,7 @@ GCB_SynthAudioProcessorEditor::~GCB_SynthAudioProcessorEditor()
 void GCB_SynthAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+   // g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
@@ -48,13 +57,16 @@ void GCB_SynthAudioProcessorEditor::paint (juce::Graphics& g)
 
 void GCB_SynthAudioProcessorEditor::resized()
 {
-    // Imposto le dimensioni della tastiera MIDI
     int keyboardHeight = 100;
+    auto leftMargin = getWidth() * 0.02;
+    auto topMargin = keyboardHeight + (getHeight() - keyboardHeight) * 0.25; // Spazio sopra la tastiera MIDI
+    auto knobSize = getWidth() * 0.2;
+
+    // Imposto le dimensioni della tastiera MIDI
     keyboardComp.setBounds(0, 0, getWidth(), keyboardHeight);
 
     // Imposto le dimensioni e la posizione dello slider
-    auto leftMargin = getWidth() * 0.02;
-    auto topMargin = keyboardHeight + (getHeight() - keyboardHeight) * 0.04; // Spazio sopra la tastiera MIDI
-    auto knobSize = getWidth() * 0.25;
     knob.setBounds(leftMargin, topMargin, knobSize, knobSize);
+
+    border.setBounds(leftMargin, topMargin * 0.5, knobSize, knob.getY() + knob.getHeight());
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -10,6 +10,7 @@
 
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
+using namespace juce;
 
 //==============================================================================
 /**
@@ -32,6 +33,38 @@ private:
     juce::MidiKeyboardComponent keyboardComp;
 
     juce::Slider knob;
+    juce::GroupComponent border;
+
+   /*class CustomLookAndFeel : public juce::LookAndFeel_V4
+    {
+    public:
+        void drawRotarySlider(juce::Graphics& g, int x, int y, int width, int height, float sliderPos,
+            const float rotaryStartAngle, const float rotaryEndAngle, juce::Slider& slider) override
+        {
+            const float diameter = juce::jmin(width, height) - 4.0f;
+            const float radius = diameter / 2.0f;
+            const float centreX = x + width * 0.5f;
+            const float centreY = y + height * 0.5f;
+            const float rx = centreX - radius;
+            const float ry = centreY - radius;
+
+            // Disegna il cerchio esterno
+            g.setColour(juce::Colours::darkgrey);
+            g.fillEllipse(rx, ry, diameter, diameter);
+
+            // Disegna la manopola
+            const float sliderRadius = diameter * 0.4f;
+            const float angle = rotaryStartAngle + sliderPos * (rotaryEndAngle - rotaryStartAngle);
+            const float knobX = centreX + radius * std::cos(angle - juce::MathConstants<float>::halfPi);
+            const float knobY = centreY + radius * std::sin(angle - juce::MathConstants<float>::halfPi);
+            g.setColour(juce::Colours::lightgrey);
+            g.fillEllipse(knobX - sliderRadius, knobY - sliderRadius, sliderRadius * 2.0f, sliderRadius * 2.0f);
+
+            // Disegna il punto centrale
+            g.setColour(juce::Colours::white);
+            g.fillEllipse(centreX - sliderRadius * 0.2f, centreY - sliderRadius * 0.2f, sliderRadius * 0.4f, sliderRadius * 0.4f);
+        }
+    };*/ 
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (GCB_SynthAudioProcessorEditor)
 };
